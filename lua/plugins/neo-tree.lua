947b1181f0cf4bf9f559ae27336e6b363c8f540e
ianpatricck <ianpatrick499@gmail.com>
2024-10-08 19:56:08 -0300

    feat: neotree show hidden items

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,21 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+
+    local neotree = require("neo-tree")
+    neotree.setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    })
+  end,
 }
