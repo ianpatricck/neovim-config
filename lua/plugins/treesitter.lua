71ff3bc9395b527ad0c3f1898c66ebf5e8dfeb64
ianpatricck <ianpatrick499@gmail.com>
2024-10-10 13:28:21 -0300

    feat: Updated treesitter ensure_installed parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,31 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "c", "lua", "vim", "javascript", "html", "php", "python" },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {
+        "lua",
+        "vim",
+        "c",
+        "cpp",
+        "dockerfile",
+        "yaml",
+        "vue",
+        "prisma",
+        "html",
+        "css",
+        "javascript",
+        "php",
+        "php_only",
+        "phpdoc",
+        "twig",
+        "python",
+        "markdown"
+      },
+      autoinstall = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
