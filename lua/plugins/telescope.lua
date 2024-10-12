bd3928c9fa1abad6935ee57eb20718dc4d2694c1
ianpatricck <ianpatrick499@gmail.com>
2024-10-11 22:32:43 -0300

    feat: added telescope.nvim

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    {"<leader>ff", "<cmd>Telescope find_files<cr>", desc = "FindFiles"},
+    {"<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers"}
+   }
+}
