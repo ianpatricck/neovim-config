1c15d41ce4f79ca209717fbf6cad91b9dde07ce5
ianpatricck <ianpatrick499@gmail.com>
2024-10-12 10:44:42 -0300

    feat: added ToggleTerm.nvim

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,8 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "v2.12.0",
+  config = true,
+  keys = {
+    { "<C-t>", "<cmd>ToggleTerm size=60 direction=vertical<cr>", desc = "ToggleTerm" },
+  }
+}
