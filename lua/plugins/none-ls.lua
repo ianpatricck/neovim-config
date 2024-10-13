85c29dcdbcbf418c91310f2cb6baa098df6a26e6
ianpatricck <ianpatrick499@gmail.com>
2024-10-13 20:10:57 -0300

    feat: added null-ls and Mason

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,17 @@
+return {
+	"nvimtools/none-ls.nvim",
+	dependences = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+			},
+		})
+
+		vim.keymap.set("n", "<C-f>", vim.lsp.buf.format, {})
+	end,
+}
