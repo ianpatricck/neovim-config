e8bee7c5a954244794e9bbcc1906e30a8a4279d2
ianpatricck <ianpatrick499@gmail.com>
2024-10-14 18:49:33 -0300

    feat: added sql_formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,7 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
-	dependences = {
+	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"nvimtools/none-ls-extras.nvim",
 	},
 	config = function()
 		local null_ls = require("null-ls")
@@ -9,6 +10,10 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.pretty_php,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.sql_formatter,
 			},
 		})
 
