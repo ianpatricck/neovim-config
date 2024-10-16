3bce0fd92afddc96186f82ee59834f18a06ecca0
ianpatricck <ianpatrick499@gmail.com>
2024-10-16 17:17:13 -0300

    feat: added nvim-lspconfig

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,74 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		"saadparwaiz1/cmp_luasnip",
+		"L3MON4D3/LuaSnip",
+	},
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local lspconfig = require("lspconfig")
+
+		local servers = {
+			"clangd",
+			"pyright",
+      "phpactor",
+			"ts_ls",
+			"html",
+			"cssls",
+			"vuels",
+      "jsonls",
+      "prismals"
+		}
+
+		for _, lsp in ipairs(servers) do
+			lspconfig[lsp].setup({
+				capabilities = capabilities,
+			})
+		end
+
+		local luasnip = require("luasnip")
+
+		local cmp = require("cmp")
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			},
+		})
+	end,
+}
